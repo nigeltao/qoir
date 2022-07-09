0248c1738bc35d2492b7804f81c07d052b6570db
Nigel Tao <nigeltao@golang.org>
2022-07-10 09:28:02 +1000

    Remove etc_decode_tile_opcodes s0 variable
    After   0.2524 CmpRatio    149.81 EncMPixels/s    222.99 DecMPixels/s
    Before  0.2524 CmpRatio    149.73 EncMPixels/s    228.78 DecMPixels/s

--- src/qoir.h
+++ src/qoir.h
@@ -893,61 +893,64 @@ qoir_private_decode_tile_opcodes(  //
     }
 
     uint64_t s64 = qoir_private_peek_u64le(sp);
-    uint8_t s0 = *sp++;
-    if (s0 == 0xF7) {  // QOIR_OP_RGB8
-      pixel[0] += *sp++;
-      pixel[1] += *sp++;
-      pixel[2] += *sp++;
+    if ((s64 & 0xFF) == 0xF7) {  // QOIR_OP_RGB8
+      pixel[0] += (uint8_t)(s64 >> 0x08);
+      pixel[1] += (uint8_t)(s64 >> 0x10);
+      pixel[2] += (uint8_t)(s64 >> 0x18);
+      sp += 4;
       memcpy(color_cache[qoir_private_hash(pixel)], pixel, 4);
       memcpy(dp, pixel, 4);
       dp += 4;
-    } else if (s0 == 0xEF) {  // QOIR_OP_RGBA8
-      pixel[0] += *sp++;
-      pixel[1] += *sp++;
-      pixel[2] += *sp++;
-      pixel[3] += *sp++;
+    } else if ((s64 & 0xFF) == 0xEF) {  // QOIR_OP_RGBA8
+      pixel[0] += (uint8_t)(s64 >> 0x08);
+      pixel[1] += (uint8_t)(s64 >> 0x10);
+      pixel[2] += (uint8_t)(s64 >> 0x18);
+      pixel[3] += (uint8_t)(s64 >> 0x20);
+      sp += 5;
       memcpy(color_cache[qoir_private_hash(pixel)], pixel, 4);
       memcpy(dp, pixel, 4);
       dp += 4;
     } else {
-      switch (s0 & 0x03) {
+      switch (s64 & 0x03) {
         case 0: {  // QOIR_OP_INDEX
-          memcpy(pixel, color_cache[s0 >> 2], 4);
+          sp += 1;
+          memcpy(pixel, color_cache[(uint8_t)s64 >> 2], 4);
           memcpy(dp, pixel, 4);
           dp += 4;
           break;
         }
         case 1: {  // QOIR_OP_RGB2
-          pixel[0] += ((s0 >> 2) & 0x03) - 2;
-          pixel[1] += ((s0 >> 4) & 0x03) - 2;
-          pixel[2] += ((s0 >> 6) & 0x03) - 2;
+          pixel[0] += ((s64 >> 2) & 0x03) - 2;
+          pixel[1] += ((s64 >> 4) & 0x03) - 2;
+          pixel[2] += ((s64 >> 6) & 0x03) - 2;
+          sp += 1;
           memcpy(color_cache[qoir_private_hash(pixel)], pixel, 4);
           memcpy(dp, pixel, 4);
           dp += 4;
           break;
         }
         case 2: {  // QOIR_OP_LUMA
-          uint8_t s1 = *sp++;
-          uint8_t delta_g = (s0 >> 2) - 32;
-          pixel[0] += delta_g - 8 + (s1 & 0x0F);
+          uint8_t delta_g = ((uint8_t)s64 >> 2) - 32;
+          pixel[0] += delta_g - 8 + ((s64 >> 0x08) & 0x0F);
           pixel[1] += delta_g;
-          pixel[2] += delta_g - 8 + (s1 >> 4);
+          pixel[2] += delta_g - 8 + ((s64 >> 0x0C) & 0x0F);
+          sp += 2;
           memcpy(color_cache[qoir_private_hash(pixel)], pixel, 4);
           memcpy(dp, pixel, 4);
           dp += 4;
           break;
         }
         case 3: {
-          if ((s0 & 0x04) == 0) {  // QOIR_OP_RGB7 given 0x03 bits are set.
+          if ((s64 & 0x04) == 0) {  // QOIR_OP_RGB7 given 0x03 bits are set.
             pixel[0] += ((s64 >> 0x03u) & 0x7F) - 0x40;
             pixel[1] += ((s64 >> 0x0Au) & 0x7F) - 0x40;
             pixel[2] += ((s64 >> 0x11u) & 0x7F) - 0x40;
-            sp += 2;
+            sp += 3;
             memcpy(color_cache[qoir_private_hash(pixel)], pixel, 4);
             memcpy(dp, pixel, 4);
             dp += 4;
-          } else if (s0 < 0xD7) {  // QOIR_OP_RUNS
-            size_t run_length = s0 >> 3;
+          } else if ((s64 & 0xFF) < 0xD7) {  // QOIR_OP_RUNS
+            size_t run_length = (s64 & 0xFF) >> 3;
             if ((dq - dp) < (4 * (run_length + 1))) {
               result.status_message = qoir_status_message__error_invalid_data;
               return result;
@@ -956,8 +959,9 @@ qoir_private_decode_tile_opcodes(  //
               memcpy(dp, pixel, 4);
               dp += 4;
             } while (run_length--);
-          } else if (s0 == 0xD7) {  // QOIR_OP_RUNL
-            size_t run_length = *sp++;
+            sp += 1;
+          } else if ((s64 & 0xFF) == 0xD7) {  // QOIR_OP_RUNL
+            size_t run_length = (s64 >> 8) & 0xFF;
             if ((dq - dp) < (4 * (run_length + 1))) {
               result.status_message = qoir_status_message__error_invalid_data;
               return result;
@@ -966,26 +970,28 @@ qoir_private_decode_tile_opcodes(  //
               memcpy(dp, pixel, 4);
               dp += 4;
             } while (run_length--);
-          } else if (s0 == 0xDF) {  // QOIR_OP_RGBA2
+            sp += 2;
+          } else if ((s64 & 0xFF) == 0xDF) {  // QOIR_OP_RGBA2
             pixel[0] += ((s64 >> 0x08u) & 0x03) - 2;
             pixel[1] += ((s64 >> 0x0Au) & 0x03) - 2;
             pixel[2] += ((s64 >> 0x0Cu) & 0x03) - 2;
             pixel[3] += ((s64 >> 0x0Eu) & 0x03) - 2;
-            sp += 1;
-            memcpy(color_cache[qoir_private_hash(pixel)], pixel, 4);
-            memcpy(dp, pixel, 4);
-            dp += 4;
-          } else if (s0 == 0xE7) {  // QOIR_OP_RGBA4
-            pixel[0] += ((s64 >> 0x08u) & 0x0F) - 8;
-            pixel[1] += ((s64 >> 0x0Cu) & 0x0F) - 8;
-            pixel[2] += ((s64 >> 0x10u) & 0x0F) - 8;
-            pixel[3] += ((s64 >> 0x14u) & 0x0F) - 8;
             sp += 2;
             memcpy(color_cache[qoir_private_hash(pixel)], pixel, 4);
             memcpy(dp, pixel, 4);
             dp += 4;
+          } else if ((s64 & 0xFF) == 0xE7) {  // QOIR_OP_RGBA4
+            pixel[0] += ((s64 >> 0x08u) & 0x0F) - 8;
+            pixel[1] += ((s64 >> 0x0Cu) & 0x0F) - 8;
+            pixel[2] += ((s64 >> 0x10u) & 0x0F) - 8;
+            pixel[3] += ((s64 >> 0x14u) & 0x0F) - 8;
+            sp += 3;
+            memcpy(color_cache[qoir_private_hash(pixel)], pixel, 4);
+            memcpy(dp, pixel, 4);
+            dp += 4;
           } else {  // QOIR_OP_A8
-            pixel[3] = *sp++;
+            pixel[3] = (uint8_t)(s64 >> 0x08);
+            sp += 2;
             memcpy(color_cache[qoir_private_hash(pixel)], pixel, 4);
             memcpy(dp, pixel, 4);
             dp += 4;
